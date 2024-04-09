3e1c157350ac440397d5edd0352a4f6f559e33e3
project <rajulamani9888@gmail.com>
2024-04-09 13:24:51 +0530

    get MinMax

---
+++ MinMax.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Pair{
+    public int Min;
+    public int Max;
+}
+
+public class MinMax {
+   static Pair Getminmax(int[] arr,int n){
+        Pair minmax = new Pair();
+        Arrays.sort(arr);
+        minmax.Min = arr[0];
+        minmax.Max = arr[n-1];
+        return minmax;
+   } 
+
+   public static void main(String[] args) {
+    int[] arr ={2,5,1,6,7,9};
+    int n = arr.length;
+    Pair minmax = Getminmax(arr,n);
+    System.out.println(minmax.Max + " " + minmax.Min);
+   }
+}
